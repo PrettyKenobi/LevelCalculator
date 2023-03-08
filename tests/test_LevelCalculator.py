d69d78ff01d6561e95eff4b51fc90a3ecf9a2028
Ken Bonnstetter <prettykenobi@gmail.com>
2023-03-07 18:15:03 -0600

    Refactor so using src/ directory structure.

--- tests/test_LevelCalculator.py
+++ tests/test_LevelCalculator.py
@@ -1,14 +1,12 @@
 import pytest
 
-from level_calculator import *
+from level_calculator import LevelCalculator
 
+def test_IncreaseLevelByOne():
+    newExperience = 20
+    calculate = LevelCalculator(1, 0)
 
-class Test_LevelCalculator:
-    def test_IncreaseLevelByOne(self):
-        newExperience = 20
-        calculate = LevelCalculator(1, 0)
+    calculate.overall_experience = newExperience
+    calculate.calculate_new_level()
 
-        calculate.overall_experience = newExperience
-        calculate.calculate_new_level()
-
-        assert calculate.current_level == 2
+    assert calculate.current_level == 2
